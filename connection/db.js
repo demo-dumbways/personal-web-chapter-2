05ce898feea70d6f36d2d2b1d76c6437a0e9071b
= <=>
2022-02-14 20:41:56 +0700

    first commit

--- connection/db.js
+++ connection/db.js
@@ -1,23 +1,23 @@
 const { Pool } = require('pg')
 
-// const dbPool = new Pool({
-//     database: 'personal_web_b29',
-//     port: 5432,
-//     user: 'postgres',
-//     password: 'root'
-// })
-
-// module.exports = dbPool
-
 const isProduction = process.env.NODE_ENV === "production";
+let pool
 
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+if (isProduction) {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
+} else {
 
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
+    pool = new Pool({
+        database: 'db_blog',
+        port: 5432,
+        user: 'postgres',
+        password: 'root'
+    })
 
-module.exports = pool;
+}
+module.exports = pool
